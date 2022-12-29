5e09bb3c05d0d9e663fc5d4f3c7f2ef896530dd7
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-29 15:22:12 +0530

    Create File struct and impl block

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 #![allow(unused_variables)]
 
-type File = String;
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new()
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -10,14 +39,19 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
-}
-
 fn main() {
-    let mut f1 = File::from("f1.txt");
-    open(&mut f1);
-    // read(&mut f1, &mut vec![]);
-    close(&mut f1);
+    let f3_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f3 = File::new_with_data("2.txt", &f3_data);
+
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f3);
+    let f3_length = f3.read(&mut buffer);
+    close(&mut f3);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{}", text);
 }
