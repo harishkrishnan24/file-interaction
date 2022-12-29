75afcbe9f65ad3ef48f83c4a653628f63f2fe54c
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-29 16:58:18 +0530

    Add file state enum

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,29 @@
-use rand::prelude::*;
-
-fn one_in(denominator: u32) -> bool {
-    thread_rng().gen_ratio(1, denominator)
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
 }
 
 #[derive(Debug)]
 struct File {
     name: String,
-    data: Vec<u8>
+    data: Vec<u8>,
+    state: FileState,
 }
 
 impl File {
     fn new(name: &str) -> File {
         File {
             name: String::from(name),
-            data: Vec::new()
+            data: Vec::new(),
+            state: FileState::Closed,
         }
     }
 
-    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
-        let mut f = File::new(name);
-        f.data = data.clone();
-
-        f
-    }
-
     fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
         let mut tmp = self.data.clone();
         let read_length = tmp.len();
 
@@ -35,35 +33,32 @@ impl File {
     }
 }
 
-fn open(f: File) -> Result<File, String> {
-    if one_in(10_000) {
-        let err_msg = String::from("Permission denied");
-        return Err(err_msg);
-    }
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
     Ok(f)
 }
 
-fn close(f: File) -> Result<File, String> {
-    if one_in(100_000) {
-        let err_msg = String::from("Interrupted by signal!");
-        return Err(err_msg);
-    }
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
     Ok(f)
 }
 
 fn main() {
-    let f4_data: Vec<u8> = vec![114, 117, 115, 116, 33];
-    let mut f4 = File::new_with_data("4.txt", &f4_data);
+    let mut f5 = File::new("5.txt");
 
     let mut buffer: Vec<u8> = vec![];
 
-    f4 = open(f4).unwrap();
-    let f4_length = f4.read(&mut buffer).unwrap();
-    f4 = close(f4).unwrap();
+    if f5.read(&mut buffer).is_err() {
+        println!("Error checking is working");
+    }
+
+    f5 = open(f5).unwrap();
+    let f5_length = f5.read(&mut buffer).unwrap();
+    f5 = close(f5).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f4);
-    println!("{} is {} bytes long", &f4.name, f4_length);
+    println!("{:?}", f5);
+    println!("{} is {} bytes long", &f5.name, f5_length);
     println!("{}", text);
 }
