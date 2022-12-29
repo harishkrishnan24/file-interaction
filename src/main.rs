1343615cff4e266af27f4560cdfe28118bd9f31e
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-29 14:54:10 +0530

    Add string version of file with stubs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![allow(unused_variables)]
+
+type File = String;
+
+fn open(f: &mut File) -> bool {
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
+}
+
+#[allow(dead_code)]
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
+    unimplemented!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut f1 = File::from("f1.txt");
+    open(&mut f1);
+    // read(&mut f1, &mut vec![]);
+    close(&mut f1);
 }
