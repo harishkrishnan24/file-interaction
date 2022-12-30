b2a3c93647a95764402a20be330fee2a34d4f443
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-30 14:54:33 +0530

    Add doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,61 @@
+// #![allow(dead_code)]
+//! Simulating files one step at a time.
+use std::fmt::{Display, Formatter, write};
+
 #[derive(Debug, PartialEq)]
-enum FileState {
+pub enum FileState {
     Open,
     Closed,
 }
 
+/// Represents a "file",
+/// which probably lives on a file system.
 #[derive(Debug)]
-struct File {
-    name: String,
+pub struct File {
+    pub name: String,
     data: Vec<u8>,
-    state: FileState,
+    pub state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
 }
 
 impl File {
-    fn new(name: &str) -> File {
+    /// Creates a new, empty `File`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let f = File::new("f1.txt");
+    /// ```
+    pub fn new(name: &str) -> File {
         File {
             name: String::from(name),
             data: Vec::new(),
             state: FileState::Closed,
         }
+    }
+
+    /// Returns the file's length in bytes.
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    /// Returns the file's name.
+    pub fn name(&self) -> String {
+        self.name.clone()
     }
 
     fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
@@ -44,7 +82,7 @@ fn close(mut f: File) -> Result<File, String> {
 }
 
 fn main() {
-    let mut f5 = File::new("5.txt");
+    let mut f5 = File::new("f6.txt");
 
     let mut buffer: Vec<u8> = vec![];
 
@@ -60,5 +98,5 @@ fn main() {
 
     println!("{:?}", f5);
     println!("{} is {} bytes long", &f5.name, f5_length);
-    println!("{}", text);
+    println!("{}", f5);
 }
